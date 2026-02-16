34f294e08b5122106bb74ceab21cf04d6053ce7b
Gabriel Machado <glemosmachado76@gmail.com>
2026-02-16 00:53:49 -0300

    render updates

--- services/api/src/db.ts
+++ services/api/src/db.ts
@@ -3,7 +3,11 @@ import mongoose from "mongoose";
 export async function connectDb(mongodbUri: string) {
   mongoose.set("strictQuery", true);
 
+  console.log("[db] connecting to:", mongodbUri.split("@")[1]);
+
   await mongoose.connect(mongodbUri);
+
+  console.log("[db] connected successfully");
 
   return mongoose.connection;
 }
